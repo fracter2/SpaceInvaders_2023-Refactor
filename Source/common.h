1d1f180d7d185cd2ed213a0a6ca21887bdb0b44a
Theodor <fracter.giftogod@gmail.com>
2025-12-27 16:22:56 +0100

    Remove duplicate common.h

--- Source/common.h
+++ Source/common.h
@@ -2,16 +2,9 @@
 #include "raylib.h"
 #include "raymath.h"
 
-inline float lineLength(Vector2 A, Vector2 B) //Uses pythagoras to calculate the length of a line
+inline bool pointInCircle(Vector2 circlePos, float radius, Vector2 point)	// TODO Rename to "isPointInCircle" to better reflect usage
 {
-	float length = sqrtf(pow(B.x - A.x, 2) + pow(B.y - A.y, 2));
-
-	return length;
-}
-
-bool pointInCircle(Vector2 circlePos, float radius, Vector2 point) // Uses pythagoras to calculate if a point is within a circle or not
-{
-	float distanceToCentre = lineLength(circlePos, point);
+	float distanceToCentre = Vector2Distance(circlePos, point);	// TODO Remove, redundant
 
 	if (distanceToCentre < radius)
 	{
