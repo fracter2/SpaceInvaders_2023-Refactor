e128c10f9e9b46dcbd111d5f5f82f4a36a62ce37
Theodor <fracter.giftogod@gmail.com>
2026-01-05 02:48:14 +0100

    Add todo

--- Source/main_menu.h
+++ Source/main_menu.h
@@ -5,7 +5,9 @@
 struct MainMenu : public Scene {
 	MainMenu(const std::function<void(SceneId)>& transitionFunc) noexcept;
 
-	std::function<void(SceneId)> transitionTo;
+	std::function<void(SceneId)> transitionTo;			// NOTE always return after call	
+														// TODO Make it idiot-proof, either force return OR make it safe 
+														// by delaying until after Update() is done
 
 	void Update() noexcept override;
 	void Render() const noexcept override;
