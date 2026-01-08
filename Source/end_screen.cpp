39ceebf98689febd49cdf79ea6327317a8e74d2e
Theodor <fracter.giftogod@gmail.com>
2026-01-08 22:28:23 +0100

    Simplify further in a nice, but minorly guideline-breaking way

--- Source/end_screen.cpp
+++ Source/end_screen.cpp
@@ -21,23 +21,26 @@ void EndScreen::Update() noexcept {
 void EndScreen::Render() const noexcept {
 	static constexpr int fontSize = 40;
 
-	static constexpr int promptX = 600;
-	static constexpr int promptY = 200;
-	DrawText("PRESS ENTER TO CONTINUE", promptX, promptY, fontSize, YELLOW);
+	{	// Prompt
+		static constexpr int x = 600, y = 200;
+		DrawText("PRESS ENTER TO CONTINUE", x, y, fontSize, YELLOW);
+	}
 
-	static constexpr int titleX = 50;
-	static constexpr int titleY = 100;
-	DrawText("LEADERBOARD",				titleX,	 titleY,  fontSize, YELLOW);
+	{	// Tittle
+		static constexpr int x = 50, y = 100;
+		DrawText("LEADERBOARD", x, y, fontSize, YELLOW);
+	}
 
 	for (int row = 0; const auto& entry : leaderboard->GetStats()) {
-		static constexpr int y = 140;
+		static constexpr int offsetY = 140;
 		static constexpr int rowHeight = 40;
+		const int y = offsetY + (row * rowHeight);
 
 		static constexpr int nameX = 50;
-		DrawText(entry.name.data(),				nameX, y + (row * rowHeight), fontSize, YELLOW);
-
 		static constexpr int scoreX = 350;
-		DrawText(TextFormat("%i", entry.score), scoreX, y + (row * rowHeight), fontSize, YELLOW);
+		DrawText(entry.name.data(),				nameX,  y, fontSize, YELLOW);		// TODO Merge into one drawText using std::format (alignment)
+		DrawText(TextFormat("%i", entry.score), scoreX, y, fontSize, YELLOW);
+
 		row++;
 	}
 }
