27152c41988468c4c3a5871e2d56a71b1b71c29e
Theodor <fracter.giftogod@gmail.com>
2026-01-08 18:36:29 +0100

    Remove redundant vars / comments in end_screen.h

--- Source/end_screen.h
+++ Source/end_screen.h
@@ -16,16 +16,4 @@ struct EndScreen : public Scene {					// TODO Make into a class as this is an in
 	void Render() const noexcept override;
 
 private:
-	//bool newHighScore = false;					// TODO Consider renaming to "queueNewHighscore" or "newHighscoreTip"
-	
-
-	//TEXTBOX ENTER	
-	std::string name = "";
-
-	Rectangle textBox = { 600, 500, 225, 50 };
-	bool mouseOnText = false;																// TODO Remove, used as a local variable
-
-	int framesCounter = 0;																	// TODO Move into textbox class
-
-
 };
