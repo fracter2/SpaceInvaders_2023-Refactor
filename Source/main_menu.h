8fd1a716225ee962f108dac5ea58a10221d290fc
Theodor <fracter.giftogod@gmail.com>
2026-01-08 18:33:35 +0100

    Remove todo

--- Source/main_menu.h
+++ Source/main_menu.h
@@ -5,9 +5,7 @@
 struct MainMenu : public Scene {		// TODO Make into a class as this is an invariant, and update public/private
 	MainMenu(const std::function<void(SceneId)>& transitionFunc) noexcept;
 
-	std::function<void(SceneId)> transitionTo;			// NOTE always return after call	
-														// TODO Make it idiot-proof, either force return OR make it safe 
-														// by delaying until after Update() is done
+	std::function<void(SceneId)> transitionTo;
 
 	void Update() noexcept override;
 	void Render() const noexcept override;
