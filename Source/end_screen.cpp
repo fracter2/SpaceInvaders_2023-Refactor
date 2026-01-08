f0c176d7484a6645331390c6a85dae49aa8e3d5c
Theodor <fracter.giftogod@gmail.com>
2026-01-08 23:00:30 +0100

    In end_screen.cpp simplify constexpr declaration and flagrantly disobey ES.10

--- Source/end_screen.cpp
+++ Source/end_screen.cpp
@@ -1,7 +1,7 @@
 #include "end_screen.h"
 #include "raylib.h"
-
 #include "common.h"
+
 
 EndScreen::EndScreen(const std::function<void(SceneId)>& transitionFunc, Leaderboard& lb) noexcept
 	: transitionTo(transitionFunc) 
@@ -22,6 +22,10 @@ void EndScreen::Render() const noexcept {
 	static constexpr int fontSize = 40;
 
 	{	// Prompt
+		// NOTE The cpp guidenline ES.10 states to declare only one var/const per line. 
+		// However i have opted to allow cases like this. It is only two, inherently 
+		// simple x / y that MUST be initialized (constexpr). It also aligns well with 
+		// the following line. 
 		static constexpr int x = 600, y = 200;
 		DrawText("PRESS ENTER TO CONTINUE", x, y, fontSize, YELLOW);
 	}
@@ -32,14 +36,12 @@ void EndScreen::Render() const noexcept {
 	}
 
 	for (int row = 0; const auto& entry : leaderboard->GetStats()) {
-		static constexpr int offsetY = 140;
-		static constexpr int rowHeight = 40;
+		static constexpr int offsetY = 140, rowHeight = 40;
 		const int y = offsetY + (row * rowHeight);
 
-		static constexpr int nameX = 50;
-		static constexpr int scoreX = 350;
-		DrawText(entry.name.data(),				nameX,  y, fontSize, YELLOW);		// TODO Merge into one drawText using std::format (alignment)
-		DrawText(TextFormat("%i", entry.score), scoreX, y, fontSize, YELLOW);
+		static constexpr int nameX = 50, scoreX = 350;
+		DrawText(entry.name.data(),				nameX,  y, fontSize, YELLOW);		// NOTE These cannot be merged well-enough using std::format because
+		DrawText(TextFormat("%i", entry.score), scoreX, y, fontSize, YELLOW);		// the filler doesn't take into account letter-width differences
 
 		row++;
 	}
