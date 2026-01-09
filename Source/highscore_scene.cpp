f89fafb2dabebef7c711269b9c625cfcc3d718a8
Theodor <fracter.giftogod@gmail.com>
2026-01-09 16:25:51 +0100

    In HighscoreScene::Render() move blinking underscore to separate free func

--- Source/highscore_scene.cpp
+++ Source/highscore_scene.cpp
@@ -21,6 +21,16 @@ void HighscoreScene::ParseNameInput() noexcept {
 }
 
 // Implementation
+void RenderBlinkingUnderscore(const char* cString, int x, int y, int fontSize, int framesCounter) noexcept
+{
+	static constexpr int duration = 20;
+	if ((framesCounter / duration) % 2) {						// NOTE "% 2" always gives 0 or 1, which is same as false / true
+		const int offsetX = MeasureText(cString, fontSize);
+		DrawText("_", x + offsetX, y, fontSize, MAROON);
+	}
+}
+
+
 HighscoreScene::HighscoreScene(const std::function<void(SceneId)>& transitionFunc, Leaderboard& lb) noexcept 
 	: transitionTo(transitionFunc)
 	, leaderboard(&lb)
@@ -76,21 +86,15 @@ void HighscoreScene::Render() const noexcept			// TODO Consider making a simple 
 
 	if (mouseOnText) {
 		if (name.size() < maxNameLength) {
-			// Draw blinking underscore char
-			if (((framesCounter / 20) % 2) == 0) {						// TODO Move to private func
-				DrawText("_", posX + 8 + MeasureText(name.c_str(), fontsizeMedium), posY + 12, fontsizeMedium, MAROON);	// TODO Consider DrawTextEx() to remove casts
-			}
+			RenderBlinkingUnderscore(name.c_str(), posX + 8, posY + 12, fontsizeMedium, framesCounter);
 		}
 		else {
 			// Name needs to be shorter
 			DrawText("Press BACKSPACE to delete chars...", posX, posY + 150, fontsizeSmall, YELLOW);
 		}
-
 	}
 
 	if (name.size() > 0 && name.size() < maxNameLength) {
 		DrawText("PRESS ENTER TO CONTINUE", posX, posY + 300, fontsizeMedium, YELLOW);
 	}
-
-	
 }
