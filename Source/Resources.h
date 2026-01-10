862dd9c7975379df3340047b930e30eeea5284b4
Theodor <fracter.giftogod@gmail.com>
2026-01-11 00:46:45 +0100

    Update todos

--- Source/Resources.h
+++ Source/Resources.h
@@ -1,5 +1,3 @@
-// TODO Rename file to be un-capitalized, including the .cpp
-
 #pragma once
 #include "raylib.h"
 #include <vector>
@@ -8,7 +6,7 @@ struct Resources
 {
 	Resources();
 
-	std::vector<Texture2D> shipTextures;	// Does it need to be a vector? is the size known at compile-time? PROBABLY, convert to std::array or equivolent to better reflect usage
+	std::vector<Texture2D> shipTextures;	// TODO Does it need to be a vector? is the size known at compile-time? PROBABLY, convert to std::array or equivolent to better reflect usage
 	Texture2D alienTexture;
 	Texture2D barrierTexture;
 	Texture2D laserTexture;
