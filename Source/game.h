6c2aa514ddab93959ced550b84f5d09e2a99b115
Theodor <fracter.giftogod@gmail.com>
2026-01-10 23:11:45 +0100

    Reorganize game.h to move important (interface) funcs first in structs/classes

--- Source/game.h
+++ Source/game.h
@@ -13,72 +13,76 @@
 struct Player								// TODO Consider moving to it's own file
 {											// TODO Make into class, keep constructor, Render() and Update() public
 public:
-	Vector2 position;
+	Player() noexcept;
+	void Update();
+	void Render(const Resources& res) const noexcept;
+	void GetPewd();
+
+	static constexpr bool active = true;	// NOTE only here to satisfy concept IsCollisionCircle
 	static constexpr float speed = 7;
 	static constexpr float player_y_offset = 70.0f;
 	static constexpr float radius = 50;
+
+	Vector2 position;
 	int lives = 3;
 	int activeTexture = 0;					// TODO Clarify, if this is the texture index used, name it so. Review usage and consider enum
 	float timer = 0;						// TODO Clarify
-
-	static constexpr bool active = true;	// NOTE only here to satisfy concept IsCollisionCircle
-
-	Player() noexcept;
-	void Render(const Resources& res) const noexcept;
-	void Update();
-	void GetPewd();
 };
 
 
 struct Projectile						// TODO Consider moving to it's own file
 {										// TODO Make into class
 public: 
-	// INITIALIZE PROJECTILE WHILE DEFINING IF ITS PLAYER OR ENEMY 
-	Vector2 position = { 0, 0 };			// TODO Add a constructor with position param
-	bool active = true;						// TODO Cconsider renaming to queueDelete
-	bool fromPlayer = false;				// TODO Consider refactoring away by having 2 separate Projectile vectors
-	Vector2 direction = { 0, 0 };
+	Projectile(Vector2 pos, Vector2 direction, bool fromPlayer) noexcept;
+	void Update();
+	void Render(const Resources& res) const noexcept;
+
+	void GetPewd();
+	inline Vector2 getLineStart() const { return Vector2Add(position, lineStartOffset); }	// TODO Consider adding Vector2 '+' overload in common.h
+	inline Vector2 getLineEnd() const { return Vector2Add(position, lineEndOffset); }
+
 	static constexpr float speed = 15;
 	static constexpr Vector2 lineStartOffset = { 0, 15 };
 	static constexpr Vector2 lineEndOffset = { 0, -15 };
 
-	inline Vector2 getLineStart() const { return Vector2Add(position, lineStartOffset); }	// TODO Consider adding Vector2 '+' overload in common.h
-	inline Vector2 getLineEnd() const { return Vector2Add(position, lineEndOffset); }
-	Projectile(Vector2 pos, Vector2 direction, bool fromPlayer) noexcept;
-	void Update();
-	void Render(const Resources& res) const noexcept;
-	void GetPewd();
+	// INITIALIZE PROJECTILE WHILE DEFINING IF ITS PLAYER OR ENEMY 
+	Vector2 position = { 0, 0 };			// TODO Add a constructor with position param
+	Vector2 direction = { 0, 0 };
+	bool active = true;						// TODO Cconsider renaming to queueDelete
+	bool fromPlayer = false;				// TODO Consider refactoring away by having 2 separate Projectile vectors
+
 };
 
 struct Wall 							// TODO Consider moving to it's own file
 {										// TODO Make into class
 public: 
-	Vector2 position;
-	bool active = true;
-	int health = 50;
-	static constexpr int radius = 60;
-
 	Wall(Vector2 pos) noexcept;
 	void Render(const Resources& res) const noexcept;
 	void GetPewd();
+
+	static constexpr int radius = 60;
+	
+	bool active = true;
+	Vector2 position;
+	int health = 50;
 };
 
 struct Alien							// TODO Consider moving to it's own file
 {										// TODO Make into class
 public:
-	Vector2 position = {0, 0};
-	bool active = true;
-	bool moveRight = true;				// TODO Rename to clarify it's a variable ("movingRight" or similar) as it sounds like an action (func-like)
-
-	static constexpr int speed = 2;
-	static constexpr int heightChangeOnBorderHit = 50;
-	static constexpr Color color = WHITE;
-	static constexpr float radius = 30;
-	
 	Alien(Vector2 pos) noexcept;
 	void Update(); 
 	void Render(const Resources& res) const noexcept;
 	void GetPewd();
+
+	static constexpr int speed = 2;
+	static constexpr int heightChangeOnBorderHit = 50;
+	static constexpr Color color = WHITE;		// TODO Remove, unused
+	static constexpr float radius = 30;
+
+	Vector2 position = {0, 0};
+	bool active = true;
+	bool moveRight = true;				// TODO Rename to clarify it's a variable ("movingRight" or similar) as it sounds like an action (func-like)
 };
 
 
@@ -87,20 +91,21 @@ struct Star
 	Star(Vector2 pos, float size) noexcept;
 	void Render(float offset) const noexcept;
 
-	static constexpr Color color = GRAY;
+	static constexpr Color color = GRAY;	// TODO Make local like all other use-cases of colors
+
 	Vector2 position = { 0, 0 };
 	float size = 0;
 };
 
 struct Background	// TODO Consider moving to separate file
 {
-	static constexpr float paralaxRatio = 15;
-	static_assert(paralaxRatio != 0);
-
-	std::vector<Star> Stars;					// TODO Consider making fixed-size, maybe with star-count as template
-
 	Background(int starAmount);
 	void Render(const float player_x) const noexcept;
+
+	static constexpr float paralaxRatio = 15;
+	static_assert(paralaxRatio != 0, "Avoid dividing by zero. Please");
+
+	std::vector<Star> Stars;					// TODO Consider making fixed-size, maybe with star-count as template
 };
 
 // TODO apply const, constexpr and noexcept where applicable
